d012f5839c1f7f60fd1fb58b8f81e1f1a5271d28
K M <menon_kavita@yahoo.com>
2024-08-19 09:46:43 -0500

    Chapter 29:React Part 1 Exercises

--- part1/exercises/src/components/BookList.jsx
+++ part1/exercises/src/components/BookList.jsx
@@ -1,15 +1,15 @@
 export default function BookList() {
-   let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+   let pageTitle = "List of Books to Read";
+   let book1 = "https://m.media-amazon.com/images/I/716yoYTrPXL._SL1500_.jpg";
+   let book2 = "https://m.media-amazon.com/images/I/719rsGQY50L._SL1500_.jpg";
+   let book3 = "https://m.media-amazon.com/images/I/71j880zaLZL._SY466_.jpg";
 
    return (
       <div>
          <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
+         <img src={book1} alt="The Well-Lived Life: A 103-Year-Old Doctor's Six Secrets to Health and Happiness at Every Age By Gladys McGarey" />
+         <img src={book2} alt="Worst Case Scenario: A Novel Kindle Edition by T.J. Newman" />
+         <img src={book3} alt="Laughing Without an Accent: Adventures of a Global Citizen by Firoozeh Dumas" />
       </div>      
    );
 }
