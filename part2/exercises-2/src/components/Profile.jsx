00a1a23208940d5471a431a6c89bd7199f20350c
K M <menon_kavita@yahoo.com>
2024-08-22 15:52:58 -0500

    Chapter 30: React Part 2 - Exercises

--- part2/exercises-2/src/components/Profile.jsx
+++ part2/exercises-2/src/components/Profile.jsx
@@ -1,14 +1,29 @@
 import './styles.css';
-import Button from './Button';
+import Button from './Button.jsx';
+import oceans from './oceans.json';
 
 
-function Profile()
-{
-   return(
-      <>
-         <h3>Coming Soon! Profiles of Ocean creatures</h3>
+
+function Profile() {
+   const listItem = oceans.map(ocean =>
+      <div key={ocean.id} className={`${ocean.fishCheck === "true" ? "isAFish" : "profile"}`}>
+         <img src={ocean.image} alt={ocean.name} className="img" />
+         <h1>{ocean.name}</h1>
+         <h2>Fun Facts:</h2>
+         <ol>
+            <li>{ocean.fact1}</li>
+            <li>{ocean.fact2}</li>
+            <li>{ocean.fact3}</li>
+         </ol>
          <Button />
-      </>  
+      </div>
+   );
+
+   return (
+      <ul>
+         {listItem}
+      </ul>
+
    );
 }
 
